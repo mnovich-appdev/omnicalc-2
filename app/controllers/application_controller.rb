9e432f8226907243a646e872ddbfa3ee46dd78b3
Matt-Novich <mnovich@chicagobooth.edu>
2021-04-21 19:47:14 +0000

    Initial commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
 
+  def add_form
+    render({:template=>"calculation_templates/add_form.html.erb"})
+  end
+
+  def subtract_form
+    render({:template=>"calculation_templates/subtract_form.html.erb"})
+  end
+
+  def multiply_form
+    render({:template=>"calculation_templates/multiply_form.html.erb"})
+  end
+
+  def divide_form
+    render({:template=>"calculation_templates/divide_form.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get("/add", {:controller=>"application", :action=>"add_form"})
+  get("/subtract", {:controller=>"application", :action=>"subtract_form"})
+  get("/multiply", {:controller=>"application", :action=>"multiply_form"})
+  get("/divide", {:controller=>"application", :action=>"divide_form"})
+
 end
